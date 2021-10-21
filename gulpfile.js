f21b70f775f274412082de27e79c7fb1db43f280
Mikita <titorovichnikita@gmail.com>
2021-10-21 14:05:11 +0200

    .

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const sass = require('gulp-sass')(require('sass'));
 function buildStyles() {
   return gulp.src('./src/scss/main.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist/css'));
+    .pipe(gulp.dest('./public/css'));
 };
 
 
