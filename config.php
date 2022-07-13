3153d5013e391549dc53b68f47e02126049e5245
Mikita Citarovič <titorovichnikita@gmail.com>
2022-07-13 11:04:09 +0200

    Update config.php

--- config.php
+++ config.php
@@ -11,7 +11,7 @@ define('ROOT', __DIR__ );
 define('BASE_URL', 'http://hrecept.cz');
 
 # DB
-define('DBHOST', 'remotemysql.com');
-define('DBNAME', 'mR0KM36tCK');
-define('DBUSER', 'mR0KM36tCK');
-define('DBPASSWORD', 'Z8QDmiEx0h');
+define('DBHOST', '');
+define('DBNAME', '');
+define('DBUSER', '');
+define('DBPASSWORD', '');
