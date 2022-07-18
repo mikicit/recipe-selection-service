5d65f8fabc0b0e06e450e111382cf77f91ff597b
Mikicit <titorovichnikita@gmail.com>
2022-07-19 01:07:23 +0200

    Deploy on Heroku

--- index.php
+++ index.php
@@ -8,9 +8,9 @@
  */
 
 ## PHP settings
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+//ini_set('error_reporting', E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
 
 ini_set('session.gc_maxlifetime', 86400);
 ini_set('session.cookie_lifetime', 0);
